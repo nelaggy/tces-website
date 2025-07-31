e203bce13e8fa883c120162a509c5e83037f2e60
Galen Lee <galen@MacBook-Pro.local>
2025-07-31 22:28:24 +0800

    Added PostList unemployment

--- src/lib/components/blog/PostCard.svelte
+++ src/lib/components/blog/PostCard.svelte
@@ -5,7 +5,7 @@
 <article
   class="rounded-lg border border-gray-100 bg-white p-4 shadow-sm transition hover:shadow-lg sm:p-6"
 >
-  <span class="inline-block rounded bg-sky-800 p-2 text-white">
+  <!-- <span class="inline-block rounded bg-sky-800 p-2 text-white">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       class="size-6"
@@ -24,15 +24,15 @@
         d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
       />
     </svg>
-  </span>
+  </span> -->
 
   <a href={href}>
-    <h3 class="mt-0.5 text-lg font-medium text-gray-900">
+    <h3 class="mt-0.5 text-lg font-medium text-gray-900 line-clamp-1">
         {title}
     </h3>
   </a>
 
-  <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+  <p class="mt-2 text-sm/relaxed text-gray-500 line-clamp-3">
     {preview}
   </p>
 
