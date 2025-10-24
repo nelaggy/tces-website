476914a01418e60fe7ccef2cd47f05395474929c
nelaggy <qgl20@cam.ac.uk>
2025-10-24 17:19:46 +0100

    slight change to pri button

--- src/lib/components/ui/PriButton.svelte
+++ src/lib/components/ui/PriButton.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import type { Snippet } from "svelte";
 
-    let { children, href } : { children: Snippet, href: string } = $props();
+    let { children, href, onclick } : { children: Snippet, href?: string, onclick?: () => void } = $props();
 </script>
 
+{#if href}
 <a
     class="block w-full rounded bg-sky-800 px-12 py-3 text-sm font-medium text-white shadow hover:bg-sky-900 focus:outline-none focus:ring active:bg-sky-700 sm:w-auto"
     href={href}
 >
     {@render children()}
 </a>
+{:else}
+<button
+    class="block w-full rounded bg-sky-800 px-12 py-3 text-sm font-medium text-white shadow hover:bg-sky-900 focus:outline-none focus:ring active:bg-sky-700 sm:w-auto"
+    onclick={onclick}
+>
+    {@render children()}
+</button>
+{/if}
